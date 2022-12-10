814578e2f3d60bdc9dbc673999f1e69c1e28d771
Сергей Николаевич <tukmachev.18@yandex.ru>
2022-12-10 18:07:20 +0300

    Задача решена!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        for (int i = 2; i < arr.length; i++) {
+            if (arr[i] % 2 == 0 && arr[i] > 2 && arr[i] < 8) {
+                arr[i]++;
+                System.out.println(arr[i]);
+            }else if
+            (arr[i] > 2 && arr[i] < 8) {
+                System.out.println(arr[i]);
+            }
+
+        }
+return;
     }
 }
